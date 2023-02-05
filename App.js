171597f44f2e02fd85b06b5f1043bc3fd7629a8c
marvin evins <marvinevins@yahoo.com>
2023-02-05 06:57:16 -0600

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Home from './src/Screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <Home />
+      </SafeAreaView>
+    </>
   );
 }
-
+ // "main": "node_modules/expo/AppEntry.js",
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ src/Screens/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const Home = () => {
+    return (
+        <View >
+        <Text>Home</Text>
+        <Ionicons name="ios-checkmark-circle" size={32} color="green" />
+        </View>
+    );
+    }
+
+    export default Home;
